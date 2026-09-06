4963b73a851b604e56fa36c2c4279e917ba86be4
takecare-77 <mverse953@gmail.com>
2026-09-06 10:36:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,98 +14,50 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const CHAT_SECRET_PASSWORD = "1717";
-const VAULT_PIN = "1717";
-
 let liveMessages = [];
-let deletedMessages = [];
+
+const VALID_USERS = {
+    "user1": "pass123",
+    "user2": "pass456"
+};
 
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
-    if (username && password === CHAT_SECRET_PASSWORD) {
+    if (VALID_USERS[username] && VALID_USERS[username] === password) {
         return res.json({ success: true });
     }
-    return res.status(400).json({ success: false, message: 'Invalid Password!' });
+    return res.status(400).json({ success: false, message: 'Invalid Credentials' });
 });
 
 io.on('connection', (socket) => {
-    let activeUser = '';
-
     socket.on('joinRoom', (username) => {
-        activeUser = username;
-        io.emit('userStatus', { user: activeUser, status: 'Online' });
         socket.emit('initialLiveMessages', liveMessages);
+    });
+
+    socket.on('requestVaultHistory', () => {
+        socket.emit('vaultHistoryData', liveMessages);
     });
 
     socket.on('chatMessage', (data) => {
         const msg = {
             id: 'msg_' + Date.now(),
             user: data.user,
-            text: data.text || '',
+            text: data.text || null,
             fileType: data.fileType || null,
             fileData: data.fileData || null,
-            replyTo: data.replyTo || null,
-            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-            status: 'sent'
+            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
         };
         liveMessages.push(msg);
         io.emit('message', msg);
     });
 
-    socket.on('editMessage', (data) => {
-        const m = liveMessages.find(item => item.id === data.id);
-        if (m && m.user === data.user) {
-            m.text = data.newText;
-            io.emit('updateMessages', liveMessages);
-        }
-    });
-
-    socket.on('deleteMessage', (msgId) => {
-        liveMessages = liveMessages.filter(item => item.id !== msgId);
-        io.emit('updateMessages', liveMessages);
-    });
-
-    socket.on('markSeen', (username) => {
-        liveMessages.forEach(m => {
-            if (m.user !== username) m.status = 'seen';
-        });
-        io.emit('seenUpdate');
-    });
-
-    socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', data);
-    });
-
-    socket.on('callUser', (data) => {
-        socket.broadcast.emit('incomingCall', { from: data.from, signalData: data.signalData, callType: data.callType });
-    });
-
-    socket.on('acceptCall', (data) => {
-        socket.broadcast.emit('callAccepted', data.signal);
-    });
-
-    socket.on('endCallSignal', () => {
-        socket.broadcast.emit('callEnded');
-    });
-
-    socket.on('moveToDeletedBox', () => {
-        if (liveMessages.length > 0) {
-            deletedMessages.push(...liveMessages);
-            liveMessages = [];
-            io.emit('liveChatCleared');
-        }
-    });
-
-    socket.on('openDeletedBox', (pin) => {
-        if (pin === VAULT_PIN) {
-            socket.emit('deletedBoxData', deletedMessages);
-        } else {
-            socket.emit('vaultError', 'Incorrect PIN code!');
-        }
-    });
+    // WebRTC Signaling for Calls
+    socket.on('offer', (data) => socket.broadcast.emit('offer', data));
+    socket.on('answer', (data) => socket.broadcast.emit('answer', data));
+    socket.on('candidate', (data) => socket.broadcast.emit('candidate', data));
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
